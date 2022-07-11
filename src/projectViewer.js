3a0174afa82c0aaeba2b472fb2d4e62c913e0a65
KyleK <kyleknot@outlook.com>
2022-07-11 13:22:21 -0700

    Added link to portfolio Github

--- src/projectViewer.js
+++ src/projectViewer.js
@@ -5,7 +5,7 @@ export class ProjectViewer extends React.Component {
         super(props);
         this.state = {
             projectArray: [
-                ['Digital Portfolio', 'July 2022', "A collection of information and links that help people interested in my work to get to know me better. It was built using react and javascript.", null, null],
+                ['Digital Portfolio', 'July 2022', "A collection of information and links that help people interested in my work to get to know me better. It was built using react and javascript.", null, "https://github.com/knot427/KyleKnotsPortfolio"],
                 ['Giselle Generator', 'June 2022', "A convolutional generative adversarial network that generates 32X32 colour images of my girlfriends face. It was built using python and trained on a dataset of handpicked photos from my phone.", null, "https://github.com/knot427/Giselle-Generator"],
                 ['Course Querier', 'April 2022', "A fullstack application that involved a front end written in pure HTML allowing a user to communicate with a server using a REST API to perform complex data querying on various datasets. The backend server was written using typescript and was able to handle large amounts of data, complex multi-stage queries, and backing up to disc. Unfortunately due to it being a school project I do not have the right to share a link to the code for the project.", null, null],
                 ['RANSAC Object Recognition System', 'March 2022', "A python script that could recognise objects in an image from a database using the RANSAC technique. Unfortunately due to it being a school project I do not have the right to share a link to the code for the project.", null, null],
